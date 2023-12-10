96ba3d2f3984ef02e02a24ba99538630d942d244
Charles <charles.bridger@gmail.com>
2023-12-10 18:41:38 +1300

    use more readable syntax

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -23,56 +23,56 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      const isAgedBrie = this.items[i].name == AGED_BRIE;
-      const isBackstagePasses = this.items[i].name == BACKSTAGE_PASSES;
-      const isSulfuras = this.items[i].name == SULFURAS;
-      const isConjured = this.items[i].name.includes(CONJURED);
+    for (let item of this.items) {
+      const isAgedBrie = item.name == AGED_BRIE;
+      const isBackstagePasses = item.name == BACKSTAGE_PASSES;
+      const isSulfuras = item.name == SULFURAS;
+      const isConjured = item.name.includes(CONJURED);
       if (!isAgedBrie && !isBackstagePasses) {
-        if (this.items[i].quality > 0) {
+        if (item.quality > 0) {
           if (!isSulfuras) {
-            this.items[i].quality = this.items[i].quality - 1;
+            item.quality = item.quality - 1;
           }
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
           if (isBackstagePasses) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
       if (!isSulfuras) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        item.sellIn = item.sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
+      if (item.sellIn < 0) {
         if (!isAgedBrie) {
           if (!isBackstagePasses) {
-            if (this.items[i].quality > 0) {
+            if (item.quality > 0) {
               if (!isSulfuras && !isConjured) {
-                this.items[i].quality = this.items[i].quality - 1;
+                item.quality = item.quality - 1;
               } else if (isConjured) {
                 //Conjured items degrade in Quality twice as fast as normal items (4 vs 2 of normal items)
-                this.items[i].quality = Math.max(this.items[i].quality - 3, 0);
+                item.quality = Math.max(item.quality - 3, 0);
               }
             }
           } else {
             //Back stage pases quality drops to 0 after the concert
-            this.items[i].quality = 0;
+            item.quality = 0;
           }
         } else {
-          if (this.items[i].quality < 50) {
+          if (item.quality < 50) {
             //Aged Brie increases in quality the older it gets
-            this.items[i].quality = this.items[i].quality + 1;
+            item.quality = item.quality + 1;
           }
         }
       }
